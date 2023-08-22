bfd686b4c7d724316e5de2649e09295bed817249
Roshith K T <123396675+Roshith-KT@users.noreply.github.com>
2023-08-22 20:17:52 +0530

    generate pdf file functionality completed

--- pdfapp/urls.py
+++ pdfapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+app_name="pdfapp"
 urlpatterns =[
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('pdf_file/<int:id>',views.generate_pdf_file,name="pdf_file"),
 ]
--- pdfapp/views.py
+++ pdfapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from . models import Item
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import os
 
 # Create your views here.
 
@@ -9,3 +14,40 @@ def index(request):
         "items": items
     }
     return render(request, 'index.html', context)
+
+
+def generate_pdf_file(request,id):
+    try:
+        item=Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        return HttpResponse("505 Not Found")
+    
+    data={
+        'item_name':item.name,
+        'item_qty':item.quantity,
+    }
+
+    pdf=render_to_pdf("pdf.html", data)
+    return HttpResponse(pdf,content_type='application/pdf')
+
+    #force download pdf file code
+    if pdf:
+        response=HttpResponse(pdf,content_type='application/pdf')
+        filename="Item_%s.pdf" %(data['item_name'])
+        content="inline; filename='%s'" %(filename)
+        content="attachment; filename=%s" %(filename)
+        response['Content-Disposition']=content
+        return response
+    return HttpResponse("Not found")
+
+
+
+def render_to_pdf(template_src,context_dict:dict):
+    template=get_template(template_src)
+    html=template.render(context_dict)
+    result=BytesIO()
+    pdf=pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")),result)
+
+    if not pdf.err:
+        return HttpResponse(result.getvalue(),content_type='application/pdf')
+    return None
